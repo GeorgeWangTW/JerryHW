182e68900ad31021c449f9f9709b271f5988c64e
GeorgeWang26805031 <cool26805031@gmail.com>
2018-03-28 14:43:07 +0800

    0326
    0326

---
+++ 0326/source/calculator.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def calculator(a,b,c):
+    if(c=="+"):
+        return(int(a)+int(b))
+    if(c=="-"):
+        return(int(a)-int(b))
+    if(c=="*"):
+        return(int(a)*int(b))
+    if(c=="/"):
+        return(int(a)/int(b))
+
